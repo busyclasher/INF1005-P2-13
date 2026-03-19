3e6322267a20b1ff9479d304b4dc5767f8b37bde
jingyu <tjingyu18@gmail.com>
2026-03-20 00:56:03 +0800

    revert db_config file back values for remote testing

--- backend/db_config.php
+++ backend/db_config.php
@@ -1,13 +1,14 @@
 <?php
 // Database connection configuration
 
-$db_host = 'localhost';
+$db_host = '127.0.0.1';
+$db_port = 3306;
 $db_username = 'inf1005-sqldev';
 $db_password = 'KkUufX74-6hM';
 $db_name = 'assignment';
 
 // Create connection
-$conn = new mysqli($db_host, $db_username, $db_password, $db_name);
+$conn = new mysqli($db_host, $db_username, $db_password, $db_name, $db_port);
 
 // Check connection
 if ($conn->connect_error) {
